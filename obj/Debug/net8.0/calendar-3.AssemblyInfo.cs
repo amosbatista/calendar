ed2705008c32b2cd863658bd8a65a780fd2cca4e
Amós Batista <amos.silva@gmail.com>
2024-08-21 10:35:09 -0300

    chore: serviços excluídos

--- obj/Debug/net8.0/calendar-3.AssemblyInfo.cs
+++ obj/Debug/net8.0/calendar-3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calendar-3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffec0f084b5cf168c9191fb1dc0d48e159d5d05d")]
 [assembly: System.Reflection.AssemblyProductAttribute("calendar-3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calendar-3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
